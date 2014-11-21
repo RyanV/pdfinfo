18ec6ee507bc0a9dee2ba24b57eb1fff55341a5e
Ryan Venegas <RVenegas2@Gmail.com>
2014-11-20 23:36:14 -0800

    fix xpdf install script

--- script/install-xpdf.sh
+++ script/install-xpdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 tar -xzvf install/xpdf-3.04.tar.gz
-cd install/xpdf-3.04 && ./configure --prefix=/usr && make && sudo make install
+cd xpdf-3.04 && ./configure --prefix=/usr && make && sudo make install
