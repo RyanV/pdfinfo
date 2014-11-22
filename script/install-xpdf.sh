f1070ede06df5dfc901064eacb2f663d619c18cc
Ryan Venegas <RVenegas2@Gmail.com>
2014-11-21 20:26:59 -0800

    update xpdf-install script

--- script/install-xpdf.sh
+++ script/install-xpdf.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-
-: ${XPDF_VERSION:= "3.04"}
-
-xpdftar="install/xpdf-$XPDF_VERSION.tar.gz"
-
 set -ex
-
-if [[ -f $xpdftar ]]; then
-  tar -xzvf $xpdftar
-  cd xpdf-$XPDF_VERSION && ./configure --prefix=/usr && make && sudo make install
-else
-  echo "xpdf install file not found '$xpdf_tar'" >&2
-  exit 1
-fi
-
+tar -xzvf install/xpdf-$XPDF_VERSION.tar.gz
+cd xpdf-$XPDF_VERSION && ./configure --prefix=/usr && make && sudo make install
