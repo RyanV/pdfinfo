541c4f5e523d2803fa4a684771296796fe0e87ff
Ryan Venegas <RVenegas2@Gmail.com>
2014-11-21 19:59:00 -0800

    update travis config to build ruby 1.8.7 and xpdf-1.0.1 and 2.0.3

--- script/install-xpdf.sh
+++ script/install-xpdf.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+: ${XPDF_VERSION:= "3.04"}
+xpdf_tar = "install/xpdf-$XPDF_VERSION"
+
 set -ex
-tar -xzvf install/xpdf-3.04.tar.gz
-cd xpdf-3.04 && ./configure --prefix=/usr && make && sudo make install
+if [[ -f $xpdf_tar ]]; then
+  tar -xzvf install/xpdf-3.04.tar.gz
+  cd xpdf-3.04 && ./configure --prefix=/usr && make && sudo make install
+else
+  echo "xpdf install file not found '$xpdf_tar'" >&2
+  exit 1
+fi
+
