1c6284eed3e6e5b78074456fef8d578ab0d7b92a
Ryan Venegas <RVenegas2@Gmail.com>
2014-11-23 02:16:52 -0800

    update travis yaml and xpdf install script to only build 1.9.3, 2.1.2 ruby builds.  no XPDF_VERSION

--- script/install-xpdf.sh
+++ script/install-xpdf.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -ex
-: ${XPDF_VERSION:="3.04"}
-tar -xzvf install/xpdf-$XPDF_VERSION.tar.gz
-cd xpdf-$XPDF_VERSION && ./configure --prefix=/usr && make && sudo make install
+tar -xzvf install/xpdf-3.04.tar.gz
+cd xpdf-3.04 && ./configure --prefix=/usr && make && sudo make install
