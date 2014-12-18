d8769033c0111eb423ea7e12b8a7363e96e3bd2e
Ryan Venegas <RVenegas2@Gmail.com>
2014-12-18 05:59:28 -0800

    ruby 2.1.2 bugfixes

--- lib/pdfinfo/object_to_hash.rb
+++ lib/pdfinfo/object_to_hash.rb
@@ -1,19 +1,19 @@
 class Array
-  def to_h
-    map {|v| v.respond_to?(:to_h) ? v.to_h : v }
-  end unless method_defined?(:to_h)
+  def as_json
+    map {|v| v.respond_to?(:as_json) ? v.as_json : v }
+  end unless method_defined?(:as_json)
 end
 
 class Pdfinfo
   module ObjectToHash
-    def to_h
+    def as_json
       instance_variables.inject({}) do |hash, var|
         val = instance_variable_get(var)
-        hash[var[1..-1].to_sym] = (val.respond_to?(:to_h) ? val.to_h : val)
+        hash[var[1..-1].to_sym] = (val.respond_to?(:as_json) ? val.as_json : val)
         hash
       end
     end
-    alias to_hash to_h
-    alias as_json to_h
+    alias to_hash as_json
+    alias to_h as_json
   end
 end
